4f35febb3a3ec829854bb2e54a14d22938bd19f1
dwang.hello <dwang.hello@outlook.com>
2016-08-11 13:43:39 +0800

    测试事件

--- DWSwipeGesture/DWSwipeGesture/ViewController.m
+++ DWSwipeGesture/DWSwipeGesture/ViewController.m
@@ -8,20 +8,69 @@
 
 #import "ViewController.h"
 
+#import "DWSwipeGestures.h"
+
 @interface ViewController ()
+
+@property (strong, nonatomic) DWSwipeGestures *swip;
+
+//蓝色View
+@property (weak, nonatomic) IBOutlet UIView *one;
+
+//红色View
+@property (weak, nonatomic) IBOutlet UIView *two;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+- (DWSwipeGestures *)swip {
+    
+    if (!_swip) {
+        
+        _swip = [[DWSwipeGestures alloc] init];
+        
+    }
+    
+    return _swip;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewDidLoad {
+    [super viewDidLoad];
+  
+    //需要进行几次点击/只有敲击手势时才使用此属性/默认为1
+    self.swip.numberOfTapsRequired = 2;
+    
+    //需要进行操作的手指数量/默认为1
+    self.swip.numberOfTouchesRequired = 2;
+    
+    //添加手势
+    [self.swip dw_SwipeGestureType:DWTapGesture Target:self Action:@selector(click) AddView:self.one BackSwipeGestureTypeString:^(NSString * _Nonnull backSwipeGestureTypeString) {
+        
+        NSLog(@"%@",backSwipeGestureTypeString);
+    
+        }];
+    
+    //添加手势
+    [self.swip dw_SwipeGestureType:DWRightSwipeGestures Target:self Action:@selector(click) AddView:self.two BackSwipeGestureTypeString:^(NSString * _Nonnull backSwipeGestureTypeString) {
+        
+        NSLog(@"%@",backSwipeGestureTypeString);
+        
+    }];
+
+        
 }
+
+- (void)click {
+    
+    NSLog(@"删除");
+    
+    NSLog(@"选择类型为第%lu个枚举值",(unsigned long)self.swip.swipeGestureType);
+    
+    //删除手势
+    [self.swip dw_RemoveSwipeGesture:DWTapGesture];
+    
+}
+
 
 @end
